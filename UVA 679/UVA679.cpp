6621c18ade6ef64b87b0e96778e2d10beb0e5b06
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-02-26 11:48:42 -0600

    updated version

--- UVA 679/UVA679.cpp
+++ UVA 679/UVA679.cpp
@@ -3,26 +3,19 @@
 
 using namespace std;
 const int maxd = 20;
-int s[1<<maxd]; 
 
 int main(){
 	int D, I, numCase;
 	scanf("%d",&numCase);
 	while (scanf("%d%d", &D, &I) == 2){
-		memset(s,0,sizeof(s));
-		int k, n = (1<<D) - 1;
-		for(int i = 0; i< I; i++){
-			k = 1;
-			for(;;){
-				s[k] = !s[k];
-				k = s[k] ?  k*2 : k*2 +1;
-				if(k>n) break;
-			}
-				
-
-		}
-		printf("%d\n",k/2);
+		int k = 1;
+		for(int i = 0;i<D-1;i++)
+			if(I%2){ k *= 2;I = (I+1)/2;}
+			else{ k = k*2 +1; I/=2;}
+		printf("%d\n", k);
 	}
+	
+	
 	
 	return 0;
 }
